f509f52ccff343684e4a965b86ff0b9e0d709f77
ljsoo123 <ljsoo123456@naver.com>
2021-03-10 17:16:32 +0900

    reducer.ts

---
+++ counter-saga/src/modules/reducer.ts
@@ -0,0 +1,17 @@
+import { INCREASE, DECREASE, CounterActionType } from "./actions";
+
+const initialState = 0;
+
+const counter = (
+  state: number = initialState,
+  action: CounterActionType
+): number => {
+  switch (action.type) {
+    case INCREASE:
+      return state + 1;
+    case DECREASE:
+      return state - 1;
+  }
+};
+
+export default counter;
