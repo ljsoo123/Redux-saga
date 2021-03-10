c7f28129b1640a8d40777fe2e8b0413cfd6bcc7d
ljsoo123 <ljsoo123456@naver.com>
2021-03-10 20:17:11 +0900

    [DELETE] delete

--- counter-saga/src/modules/actions.ts
+++
@@ -1,9 +0,0 @@
-export const INCREASE = "counter/INCREASE" as const;
-export const DECREASE = "counter/DECREASE" as const;
-
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
-
-export type CounterActionType =
-  | ReturnType<typeof increase>
-  | ReturnType<typeof decrease>;
