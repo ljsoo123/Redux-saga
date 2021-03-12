159ae06f0c0d55631b349f91cd6503c09326d248
ljsoo123 <ljsoo123456@naver.com>
2021-03-12 22:11:19 +0900

    [MODIFY] saga setting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./component/App";
+import { createBrowserHistory } from "history";
+import { createSagaMiddleware } from "redux-saga";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer, { rootSaga } from "./modules";
+import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
+import ReduxThunk from "redux-thunk";
+import rootReducer, { rootSaga } from "./modules/index";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const customHistory = createBrowserHistory();
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(ReduxThunk.withExtraArgument({ history: customHistory })),
+    sagaMiddleware,
+    logger
+  )
+);
+
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+  <Router history={customHistory}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById("root")
+);
